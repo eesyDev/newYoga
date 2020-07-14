8b6bd31de051c277c0b74b5a72e045f43e2bd1ec
Rano <khasanova.rano@gmail.com>
2020-07-14 19:25:17 +0600

    modal+tabs+timer

---
+++ js/js.js
@@ -0,0 +1,26 @@
+// {/* <input id="age" value="30"> */}
+// let age = document.getElementById('age');
+// function showUser(surname, name) {
+// alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+// showUser.apply(age, ["Gorkyi", "Maxim"]);
+
+
+class Options {
+    constructor(height, width, background, fontSize, textAlign) {
+        this.height = height;
+        this.width = width;
+        this.background = background;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+    }
+    createDiv() {
+        let elem = document.createElement('div');
+        document.body.appendChild(elem);
+        let parametres = `height:${this.height}px; width:${this.width}px; background-color:${this.background}; font-size:${this.fontSize}px; text-align${this.textAlign}`;
+        elem.style.cssText = parametres;
+    }
+}
+
+let block = new Options(200, 100, 'yellow', 15, 'center');
+block.createDiv();
