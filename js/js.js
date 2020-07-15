f56bd0e61d76b4148a7746e922523a64dfadff54
Rano <khasanova.rano@gmail.com>
2020-07-15 23:01:18 +0600

    forms -standart & JSON

--- js/js.js
+++ js/js.js
@@ -6,21 +6,21 @@
 // showUser.apply(age, ["Gorkyi", "Maxim"]);
 
 
-class Options {
-    constructor(height, width, background, fontSize, textAlign) {
-        this.height = height;
-        this.width = width;
-        this.background = background;
-        this.fontSize = fontSize;
-        this.textAlign = textAlign;
-    }
-    createDiv() {
-        let elem = document.createElement('div');
-        document.body.appendChild(elem);
-        let parametres = `height:${this.height}px; width:${this.width}px; background-color:${this.background}; font-size:${this.fontSize}px; text-align${this.textAlign}`;
-        elem.style.cssText = parametres;
-    }
-}
+// class Options {
+//     constructor(height, width, background, fontSize, textAlign) {
+//         this.height = height;
+//         this.width = width;
+//         this.background = background;
+//         this.fontSize = fontSize;
+//         this.textAlign = textAlign;
+//     }
+//     createDiv() {
+//         let elem = document.createElement('div');
+//         document.body.appendChild(elem);
+//         let parametres = `height:${this.height}px; width:${this.width}px; background-color:${this.background}; font-size:${this.fontSize}px; text-align${this.textAlign}`;
+//         elem.style.cssText = parametres;
+//     }
+// }
 
-let block = new Options(200, 100, 'yellow', 15, 'center');
-block.createDiv();
+// let block = new Options(200, 100, 'yellow', 15, 'center');
+// block.createDiv();
--- js/script.js
+++ js/script.js
@@ -119,4 +119,89 @@ window.addEventListener('DOMContentLoaded', function () {
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
+    //form
+
+    let message = {
+        loading: "Загрузка...",
+        success: "Cпасибо! Скоро мы с вами свяжемся!",
+        failure: "Что-то пошло не так..."
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
+
+        let fomData = new FormData(form);
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHtml = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+    });
+
+    //JSON 
+    // let message = {
+    //     loading: "Загрузка...",
+    //     success: "Cпасибо! Скоро мы с вами свяжемся!",
+    //     failure: "Что-то пошло не так..."
+    // };
+
+    // let form = document.querySelector('.main-form'),
+    //     input = form.getElementsByTagName('input'),
+    //     statusMessage = document.createElement('div');
+
+    //     statusMessage.classList.add('status');
+
+    // form.addEventListener('submit', function(event) {
+    //     event.preventDefault();
+    //     form.appendChild(statusMessage);
+
+    //     let request = new XMLHttpRequest();
+    //     request.open('POST', 'server.php');
+    //     request.setRequestHeader ('Content-Type', 'application/json; charset=utf-8');
+
+    //     let formData = new FormData(form);
+
+    //     let obj = {};
+    //     formData.forEach(function(value, key) {
+    //         obj[key] = value;
+    //     });
+    //     let json = JSON.stringify(obj)
+
+    //     request.send(json);
+
+    //     request.addEventListener('readystatechange', function() {
+    //         if (request.readyState < 4) {
+    //             statusMessage.innerHtml = message.loading;
+    //         } else if (request.readyState === 4 && request.status == 200) {
+    //             statusMessage.innerHTML = message.success;
+    //         } else {
+    //             statusMessage.innerHTML = message.failure;
+    //         }
+    //     });
+
+    //         for (let i = 0; i < input.length; i++) {
+    //             input[i].value = '';
+    //         }
+    // });
 });
